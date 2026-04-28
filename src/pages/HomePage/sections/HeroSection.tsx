5432589e2d3a07636115fc25ab840a3e84a2b091
lucane <contact@lucane.tech>
2026-04-28 14:40:42 +0100

    debug deploy

--- src/pages/HomePage/sections/HeroSection.tsx
+++ src/pages/HomePage/sections/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { ArrowRight, ChevronDown } from 'lucide-react'
+import { ArrowRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import Button from '@/components/ui/Button'
 import { useTranslation } from 'react-i18next'
